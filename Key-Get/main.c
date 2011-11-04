c849dff8d456eedf2a277d3f5415094f5632e164
Asher Glick <asher.glick@gmail.com>
2011-11-04 19:31:46 -0400

    fixed fake octal number

--- Key-Get/main.c
+++ Key-Get/main.c
@@ -72,7 +72,7 @@ void addNames(void) {
   namesize = 31;
   names = malloc (sizeof(int) * namesize);
   names [0] = 12345;
-  names [1] = 09876;
+  names [1] = 19876;
 }
 
 /******************************* INT0 INTERRUPT *******************************\
